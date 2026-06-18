2232d1d6a86037a4b255fde16a7ca72cf00ec595
Yeakub Ali Shakib <yashakib.dev@gmail.com>
2026-06-18 16:18:00 +0600

    Add Countdown class with countdown functionality

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,15 @@
-
+class Countdown {
+    void start(){
+        for(int i=5; i >= 1; i--){
+            System.out.println(i);
+        }
+        System.out.println("Go");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        Countdown countdown = new Countdown();
+        countdown.start();
+        
+    }
+}
